01b6afa31fb0cefe8f517627ca21c28c75f9e383
Yogesh Bamanier <bamanieryogesh1@gmail.com>
2025-03-14 00:22:14 +0530

    code clean

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,11 @@
 function Card({ className, children, id, draggable , data}) {
 
   const dragStart = (e) => {
-    // console.log("dragStart", e)
-    // e.preventDefault();
-    console.log(data)
-    
     e.dataTransfer.setData('card_id', e.target.id);
     e.dataTransfer.setData('data', JSON.stringify(data));
-
-    // setTimeout(() => {
-    //   e.target.style.display = "none";
-    
-    // }, 100);
   };
-  const dragOver = (e) => {
-    // console.log("dragOver", e)
-    e.stopPropagation();
-   
-  };
+  
+  const dragOver = (e) => e.stopPropagation();
 
   return (
     <div
